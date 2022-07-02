67e5e4a2bbafa8fa4daea725fb5686037a86ed92
Ilya Bikmetov <ilya-bikmetov@yandex.ru>
2022-07-03 00:51:02 +0700

    feat/add_popup_handlers

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,33 @@
+function handleEditProfileClick() {
+  document.querySelector('.popup_place_edit').classList.add('popup_active');
+
+}
+
+function handleAddPlaceClick() {
+  document.querySelector('.popup_place_add').classList.add('popup_active');
+
+}
+
+function handleEditAvatarClick() {
+  document.querySelector('.popup_avatar').classList.add('popup_active');
+
+}
+
 function Main() {
   return (
     <>
       <section className="profile">
         <div className="profile__user-set">
-          <div className="profile__avatar"></div>
+          <div onClick={handleEditAvatarClick} className="profile__avatar"></div>
           <div className="profile__info">
             <div className="profile__name">
               <h1 className="profile__title"></h1>
-              <button type="button" aria-label="Редактировать" className="profile__edit-button"></button>
+              <button onClick={handleEditProfileClick} type="button" aria-label="Редактировать" className="profile__edit-button"></button>
             </div>
             <p className="profile__subtitle"></p>
           </div>
         </div>
-        <button type="button" aria-label="Добавить" className="profile__add-button"></button>
+        <button onClick={handleAddPlaceClick} type="button" aria-label="Добавить" className="profile__add-button"></button>
       </section>
       <section className="elements">
         <ul className="elements__list">
